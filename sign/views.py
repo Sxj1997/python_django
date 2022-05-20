eb415519444183b4f0e67a03672a4b8be0113857
Sxj1997 <1090306920@qq.com>
2022-05-20 17:31:00 +0800

    添加index

--- sign/views.py
+++ sign/views.py
@@ -1,7 +1,32 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.contrib import auth
+from sign.models import Event
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "index.html")
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)  # 登录
+            # response.set_cookie('user', username, 3600)  # 浏览器cookie
+            request.session['user'] = username  # 读取session信息记录到浏览器
+            response = HttpResponseRedirect('/event_manage')
+            return response
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!'})
+
+# 发布会管理
+@login_required
+def event_manage(request):
+    # username = request.COOKIES.get('user', '')  # 读取浏览器cookie
+    event_list = Event.objects.all()
+    username = request.session.get('user', '')  # 读取浏览器session
+    return render(request, "event_manage.html", {'user': username, "events": event_list})
